bbb8c3fc449d37d136a64a8c14c5eda8a78b170c
fpacaud <francoispacaud8@gmail.com>
2020-12-21 23:00:08 +0100

    add documentation for preconditioners

--- src/preconditioners.jl
+++ src/preconditioners.jl
@@ -1,17 +1,54 @@
 
+"""
+    AbstractPreconditionner
+
+Abstract supertype to implement a preconditioner for
+conjugate gradient.
+"""
 abstract type AbstractPreconditionner end
 
+"""
+    update!(P::AbstractPreconditionner, B::AbstractTronMatrix, nfree, nnz, iwa, wa1, wa2)
+
+Update coefficients of preconditioner according to the new matrix `B`.
+`nfree` is the degree of freedom, and `nnz` the current number of
+elements in `B`.
+
+"""
 function update! end
-# L*y = x    &    L'*y = x
-# TODO: clean signature
+
+"""
+    dstrsol(n, P::AbstractPreconditionner, r, task)
+
+Apply preconditioner on the `n` first components of the
+current vector `r`.
+If `task` is set equal to `N`, the preconditioner is applied as is.
+If `task` is equal to `T`, then the transpose of the preconditioner operator is applied.
+
+"""
 function dstrsol end
 
-# No precond
+
+"""
+    EyePreconditionner <: AbstractPreconditionner
+
+Apply identity as preconditioning. Equivalent to no preconditioning.
+
+"""
 struct EyePreconditionner <: AbstractPreconditionner end
-update!(e::EyePreconditionner, B::TronSparseMatrixCSC, nfree, nnz, iwa, wa1, wa2) = nothing
+
+update!(e::EyePreconditionner, B::AbstractTronMatrix, nfree, nnz, iwa, wa1, wa2) = nothing
+
 dstrsol(n, P::EyePreconditionner, r, task) = nothing
 
-# IncompleteCholesky precond
+"""
+    IncompleteCholesky <: AbstractPreconditionner
+
+Use an Incomplete Cholesky Factorization routine as a preconditioner.
+The incomplete factorization is computed when calling the function
+`update!`, and the factorization stored inside the object `IncompleteCholesky`.
+
+"""
 struct IncompleteCholesky <: AbstractPreconditionner
     L::AbstractTronMatrix
     memory::Int
@@ -24,7 +61,6 @@ function update!(icfs::IncompleteCholesky, B::AbstractTronMatrix,
                  nfree, nnz, iwa, wa1, wa2)
     T = eltype(wa1)
     alpha = T(0)
-    # Compute the incomplete Cholesky factorization.
     ICFS.dicfs(
         nfree, nnz, B, icfs.L,
         icfs.nv, alpha, iwa, wa1, wa2
