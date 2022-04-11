187c922d7883d8a769e4c9502eb59d26e6c62708
Michel Schanen <Michel.Schanen@gmail.com>
2022-04-11 15:56:24 -0500

    Update README and example

--- examples/admm_standalone.jl
+++ examples/admm_standalone.jl
@@ -1,17 +1,23 @@
 using ExaTron
+using LazyArtifacts
 
-datafile = "./data/"*ARGS[1]
-pq_val = parse(Float64, ARGS[2])
-va_val = parse(Float64, ARGS[3])
-max_iter = parse(Int, ARGS[4])
-gpu = parse(Bool, ARGS[5])
+# `datafile`: the name of the test file of type `String`
+# here: MATPOWER case2868rte.m in ExaData project Artifact
+datafile = joinpath(artifact"ExaData", "ExaData", "matpower", "case2868rte.m")
+# `rho_pq`: ADMM parameter for power flow of type `Float64`
+rho_pq = 10.0
+# `rho_va`: ADMM parameter for voltage and angle of type `Float64`
+rho_va = 1000.0
+# `max_iter`: maximum number of iterations of type `Int`
+max_iter = 5000
+# `use_gpu`: indicates whether to use gpu or not, of type `Bool`
+use_gpu = true
+# Use polar formulation for branch problems
+use_polar = true
 
-println("Data: ", datafile, " rho_pq = ", pq_val, " rho_va = ", va_val, " max_iter = ", max_iter, " gpu = ", gpu)
+# Solve ACOPF
+env = ExaTron.admm_rect_gpu(datafile;
+                      iterlim=max_iter, rho_pq=rho_pq, rho_va=rho_va, scale=1e-4, use_polar=use_polar, use_gpu=use_gpu)
 
-# Warm-up
-ExaTron.admm_rect_gpu(datafile;
-                      iterlim=1, rho_pq=pq_val, rho_va=va_val, scale=1e-4, use_polar=true, use_gpu=gpu)
-
-# Run
-ExaTron.admm_rect_gpu(datafile;
-                      iterlim=max_iter, rho_pq=pq_val, rho_va=va_val, scale=1e-4, use_polar=true, use_gpu=gpu)
+# Restart and run 5000 iterations
+ExaTron.admm_restart!(env; iterlim=max_iter)
